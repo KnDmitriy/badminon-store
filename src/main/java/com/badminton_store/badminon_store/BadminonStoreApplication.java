9defb4ec9b3018ffcb7724ed7126cf38200c1cda
KnDmitriy <dmitry222003@gmail.com>
2025-01-18 13:18:00 +0400

    Initial commit

---
+++ src/main/java/com/badminton_store/badminon_store/BadminonStoreApplication.java
@@ -0,0 +1,13 @@
+package com.badminton_store.badminon_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadminonStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadminonStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/badminton_store/badminon_store/BadminonStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badminton_store.badminon_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadminonStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
